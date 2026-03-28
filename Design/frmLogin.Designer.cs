0898512b51c0bb84b5333f66c1884f8814fa9381
catalynsava <catalynsava@gmail.com>
2026-03-28 07:40:27 +0200

    login form cu TabletLayoutPanel

--- Design/frmLogin.Designer.cs
+++ Design/frmLogin.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace Ra.Design
+﻿using System.Windows.Forms;
+
+namespace Ra.Design
 {
     partial class frmLogin
     {
@@ -30,7 +32,6 @@
         {
             textUser = new TextBox();
             textPassword = new TextBox();
-            buttonLogin = new Button();
             textBazaDeDate = new TextBox();
             textServer = new TextBox();
             textPort = new TextBox();
@@ -40,115 +41,110 @@
             label4 = new Label();
             label5 = new Label();
             tableLayoutPanel1 = new TableLayoutPanel();
-            tableLayoutPanel2 = new TableLayoutPanel();
             labelLogin = new Label();
+            buttonLogin = new Button();
             tableLayoutPanel1.SuspendLayout();
-            tableLayoutPanel2.SuspendLayout();
             SuspendLayout();
             // 
             // textUser
             // 
-            textUser.Location = new Point(141, 69);
+            textUser.Anchor = AnchorStyles.None;
+            textUser.Font = new Font("Segoe UI", 9F);
+            textUser.Location = new Point(143, 77);
             textUser.Name = "textUser";
             textUser.Size = new Size(177, 27);
             textUser.TabIndex = 4;
             // 
             // textPassword
             // 
-            textPassword.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 238);
-            textPassword.Location = new Point(141, 102);
+            textPassword.Anchor = AnchorStyles.None;
+            textPassword.Font = new Font("Segoe UI", 9F);
+            textPassword.Location = new Point(143, 110);
             textPassword.Name = "textPassword";
             textPassword.PasswordChar = '●';
             textPassword.Size = new Size(177, 27);
             textPassword.TabIndex = 0;
             textPassword.KeyUp += textPassword_KeyUp;
             // 
-            // buttonLogin
-            // 
-            buttonLogin.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            buttonLogin.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 238);
-            buttonLogin.Location = new Point(245, 31);
-            buttonLogin.Name = "buttonLogin";
-            buttonLogin.Size = new Size(77, 45);
-            buttonLogin.TabIndex = 1;
-            buttonLogin.Text = "LOGIN";
-            buttonLogin.UseVisualStyleBackColor = true;
-            buttonLogin.Click += buttonLogin_Click;
-            // 
             // textBazaDeDate
             // 
-            textBazaDeDate.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 238);
-            textBazaDeDate.Location = new Point(141, 135);
+            textBazaDeDate.Anchor = AnchorStyles.Left;
+            textBazaDeDate.Font = new Font("Segoe UI", 9F);
+            textBazaDeDate.Location = new Point(141, 143);
             textBazaDeDate.Name = "textBazaDeDate";
             textBazaDeDate.Size = new Size(177, 27);
             textBazaDeDate.TabIndex = 5;
             // 
             // textServer
             // 
-            textServer.Location = new Point(141, 3);
+            textServer.Anchor = AnchorStyles.None;
+            textServer.Font = new Font("Segoe UI", 9F);
+            textServer.Location = new Point(143, 7);
             textServer.Name = "textServer";
             textServer.Size = new Size(177, 27);
             textServer.TabIndex = 2;
             // 
             // textPort
             // 
-            textPort.Location = new Point(141, 36);
+            textPort.Anchor = AnchorStyles.None;
+            textPort.Font = new Font("Segoe UI", 9F);
+            textPort.Location = new Point(143, 44);
             textPort.Name = "textPort";
             textPort.Size = new Size(177, 27);
             textPort.TabIndex = 3;
             // 
             // label1
             // 
-            label1.Anchor = AnchorStyles.Left;
             label1.AutoSize = true;
+            label1.Dock = DockStyle.Fill;
             label1.Font = new Font("Segoe UI", 9F);
-            label1.Location = new Point(3, 6);
+            label1.Location = new Point(3, 0);
             label1.Name = "label1";
-            label1.Size = new Size(51, 20);
+            label1.Size = new Size(132, 41);
             label1.TabIndex = 6;
             label1.Text = "server:";
             // 
             // label2
             // 
-            label2.Anchor = AnchorStyles.Left;
             label2.AutoSize = true;
+            label2.Dock = DockStyle.Fill;
             label2.Font = new Font("Segoe UI", 9F);
-            label2.Location = new Point(3, 39);
+            label2.Location = new Point(3, 41);
             label2.Name = "label2";
-            label2.Size = new Size(40, 20);
+            label2.Size = new Size(132, 33);
             label2.TabIndex = 7;
             label2.Text = "port:";
             // 
             // label3
             // 
-            label3.Anchor = AnchorStyles.Left;
             label3.AutoSize = true;
+            label3.Dock = DockStyle.Fill;
             label3.Font = new Font("Segoe UI", 9F);
-            label3.Location = new Point(3, 72);
+            label3.Location = new Point(3, 74);
             label3.Name = "label3";
-            label3.Size = new Size(71, 20);
+            label3.Size = new Size(132, 33);
             label3.TabIndex = 8;
             label3.Text = "utilizator:";
             // 
             // label4
             // 
-            label4.Anchor = AnchorStyles.Left;
             label4.AutoSize = true;
+            label4.Dock = DockStyle.Fill;
             label4.Font = new Font("Segoe UI", 9F);
-            label4.Location = new Point(3, 105);
+            label4.Location = new Point(3, 107);
             label4.Name = "label4";
-            label4.Size = new Size(55, 20);
+            label4.Size = new Size(132, 33);
             label4.TabIndex = 9;
             label4.Text = "parola:";
             // 
             // label5
             // 
-            label5.Anchor = AnchorStyles.Left;
             label5.AutoSize = true;
+            label5.Dock = DockStyle.Fill;
             label5.Font = new Font("Segoe UI", 9F);
             label5.Location = new Point(3, 140);
             label5.Name = "label5";
-            label5.Size = new Size(99, 20);
+            label5.Size = new Size(132, 33);
             label5.TabIndex = 10;
             label5.Text = "baza de date:";
             // 
@@ -156,8 +152,9 @@
             // 
             tableLayoutPanel1.Anchor = AnchorStyles.None;
             tableLayoutPanel1.ColumnCount = 2;
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 42.6735229F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 42.67352F));
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 57.3264771F));
+            tableLayoutPanel1.Controls.Add(labelLogin, 0, 5);
             tableLayoutPanel1.Controls.Add(textServer, 1, 0);
             tableLayoutPanel1.Controls.Add(label1, 0, 0);
             tableLayoutPanel1.Controls.Add(label5, 0, 4);
@@ -168,51 +165,50 @@
             tableLayoutPanel1.Controls.Add(label3, 0, 2);
             tableLayoutPanel1.Controls.Add(textPassword, 1, 3);
             tableLayoutPanel1.Controls.Add(label4, 0, 3);
-            tableLayoutPanel1.Location = new Point(13, 44);
+            tableLayoutPanel1.Controls.Add(buttonLogin, 1, 6);
+            tableLayoutPanel1.Location = new Point(8, 10);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
-            tableLayoutPanel1.RowCount = 5;
+            tableLayoutPanel1.RowCount = 7;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
             tableLayoutPanel1.RowStyles.Add(new RowStyle());
             tableLayoutPanel1.RowStyles.Add(new RowStyle());
             tableLayoutPanel1.RowStyles.Add(new RowStyle());
             tableLayoutPanel1.RowStyles.Add(new RowStyle());
             tableLayoutPanel1.RowStyles.Add(new RowStyle());
             tableLayoutPanel1.RowStyles.Add(new RowStyle());
-            tableLayoutPanel1.Size = new Size(325, 168);
+            tableLayoutPanel1.Size = new Size(325, 238);
             tableLayoutPanel1.TabIndex = 12;
-            // 
-            // tableLayoutPanel2
-            // 
-            tableLayoutPanel2.ColumnCount = 1;
-            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
-            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
-            tableLayoutPanel2.Controls.Add(labelLogin, 0, 0);
-            tableLayoutPanel2.Controls.Add(buttonLogin, 0, 1);
-            tableLayoutPanel2.Location = new Point(6, 248);
-            tableLayoutPanel2.Name = "tableLayoutPanel2";
-            tableLayoutPanel2.RowCount = 2;
-            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 34.02062F));
-            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 65.97938F));
-            tableLayoutPanel2.Size = new Size(325, 85);
-            tableLayoutPanel2.TabIndex = 13;
             // 
             // labelLogin
             // 
-            labelLogin.Anchor = AnchorStyles.None;
+            tableLayoutPanel1.SetColumnSpan(labelLogin, 2);
+            labelLogin.Dock = DockStyle.Fill;
             labelLogin.Font = new Font("Segoe UI", 7.8F, FontStyle.Regular, GraphicsUnit.Point, 238);
             labelLogin.ForeColor = SystemColors.ControlDarkDark;
-            labelLogin.Location = new Point(3, 0);
+            labelLogin.Location = new Point(3, 173);
             labelLogin.Name = "labelLogin";
-            labelLogin.Size = new Size(319, 28);
-            labelLogin.TabIndex = 12;
+            labelLogin.Size = new Size(319, 20);
+            labelLogin.TabIndex = 13;
             labelLogin.Text = "ⓒRegistrul Agricol V.1.283226";
             labelLogin.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // buttonLogin
+            // 
+            buttonLogin.Anchor = AnchorStyles.Right;
+            buttonLogin.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            buttonLogin.Location = new Point(245, 196);
+            buttonLogin.Name = "buttonLogin";
+            buttonLogin.Size = new Size(77, 39);
+            buttonLogin.TabIndex = 14;
+            buttonLogin.Text = "LOGIN";
+            buttonLogin.UseVisualStyleBackColor = true;
+            buttonLogin.Click += buttonLogin_Click;
             // 
             // frmLogin
             // 
             AutoScaleDimensions = new SizeF(120F, 120F);
             AutoScaleMode = AutoScaleMode.Dpi;
-            ClientSize = new Size(350, 345);
-            Controls.Add(tableLayoutPanel2);
+            ClientSize = new Size(341, 258);
             Controls.Add(tableLayoutPanel1);
             Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 238);
             MaximizeBox = false;
@@ -221,10 +217,10 @@
             ShowIcon = false;
             StartPosition = FormStartPosition.CenterScreen;
             Text = "RA";
+            TopMost = true;
             FormClosing += frmLogin_FormClosing;
             tableLayoutPanel1.ResumeLayout(false);
             tableLayoutPanel1.PerformLayout();
-            tableLayoutPanel2.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -232,7 +228,6 @@
 
         private TextBox textUser;
         private TextBox textPassword;
-        private Button buttonLogin;
         private TextBox textBazaDeDate;
         private TextBox textServer;
         private TextBox textPort;
@@ -242,7 +237,7 @@
         private Label label4;
         private Label label5;
         private TableLayoutPanel tableLayoutPanel1;
-        private TableLayoutPanel tableLayoutPanel2;
         private Label labelLogin;
+        private Button buttonLogin;
     }
 }
